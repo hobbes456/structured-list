12478b4d4f43fa4f0b104c47ae0b1dcba5024cc5
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-07-28 14:37:28 +0300

    eslint edits

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
-        })
+        }),
 });
 
 export default store;
