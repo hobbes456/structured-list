cb05c3147264f2a2e21ca9ce156e1ad62cccb0d4
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-08-07 19:26:54 +0300

    Buttons have been added to productForm

--- src/components/common/ProductForm/ProductForm.tsx
+++ src/components/common/ProductForm/ProductForm.tsx
@@ -1,5 +1,6 @@
 import FormItem from "@components/FormItem";
 import { formItems } from "@/constants/formItems";
+import { formButtons } from "@/constants/formButtons";
 
 import s from "./ProductForm.module.scss";
 
@@ -10,12 +11,15 @@ const ProductForm = () => {
                 <FormItem key={item.id} item={item} />
             ))}
             <div className={s.productForm__buttons}>
-                <button type="submit" className={s.productForm__button}>
-                    SUBMIT
-                </button>
-                <button type="reset" className={s.productForm__button}>
-                    RESET
-                </button>
+                {formButtons.map((item) => (
+                    <button
+                        key={item.type}
+                        type={item.type}
+                        className={s.productForm__button}
+                    >
+                        {item.type.toUpperCase()}
+                    </button>
+                ))}
             </div>
         </form>
     );
---
+++ src/constants/formButtons.ts
@@ -0,0 +1,12 @@
+type FormButtonsProps = {
+    type: "button" | "submit" | "reset";
+};
+
+export const formButtons: FormButtonsProps[] = [
+    {
+        type: "submit",
+    },
+    {
+        type: "reset",
+    },
+];
