99ede9a0ec0a88d227045d8143aff91657c5078f
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2024-08-21 16:06:12 +0300

    added localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { rootReducer } from "@/models";
+import { loadFromLocalStorage, saveToLocalStorage } from "./localStorage";
 
 const store = configureStore({
     reducer: rootReducer,
+    preloadedState: loadFromLocalStorage(),
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
 
---
+++ src/store/localStorage.ts
@@ -0,0 +1,25 @@
+import { RootState } from "@/store";
+
+export const saveToLocalStorage = (state: RootState): void => {
+    try {
+        const serialisedState = JSON.stringify(state);
+
+        localStorage.setItem("oldState", serialisedState);
+    } catch (error) {
+        console.warn(error);
+    }
+};
+
+export const loadFromLocalStorage = (): RootState | undefined => {
+    try {
+        const serialisedState = localStorage.getItem("oldState");
+
+        if (serialisedState === null) return undefined;
+
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+
+        return undefined;
+    }
+};
